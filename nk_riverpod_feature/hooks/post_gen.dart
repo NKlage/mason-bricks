47f03f0ca1a9d42448614b0f5e2879bed0c8f8e2
Nico Klage <nico.klage@gmx.de>
2023-05-16 12:37:35 +0200

    feat: add flutter_riverpod if needed

--- nk_riverpod_feature/hooks/post_gen.dart
+++ nk_riverpod_feature/hooks/post_gen.dart
@@ -1,7 +1,17 @@
-import 'package:mason/mason.dart';
 import 'dart:io';
 
+import 'package:mason/mason.dart';
+import 'package:yaml/yaml.dart';
+
 Future<void> run(HookContext context) async {
+  final pubspec = await File('./pubspec.yaml').readAsString();
+  print(pubspec);
+  final yaml = loadYaml(pubspec);
+
+  if (null == yaml['dependencies']['flutter_riverpod']) {
+    return;
+  }
+
   final progress = context.logger.progress('Installing Packages');
 
   await Process.run('flutter', ['pub', 'add', 'flutter_riverpod']);
