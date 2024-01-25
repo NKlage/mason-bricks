34e574aa796ffed8567584fab9f66f4c17cf18b6
Nico Klage <nico.klage@gmx.de>
2024-01-25 15:42:36 +0100

    l10 folder und route template hinzugefügt

--- nk_riverpod_feature/hooks/post_gen.dart
+++ nk_riverpod_feature/hooks/post_gen.dart
@@ -1,23 +1,24 @@
-import 'dart:io';
+// import 'dart:io';
 
 import 'package:mason/mason.dart';
-import 'package:yaml/yaml.dart';
+// import 'package:yaml/yaml.dart';
 
-Future<void> run(HookContext context) async {
-  final riverpodPackageName = 'flutter_riverpod';
-  
-  try {
-    final pubspec = await File('./pubspec.yaml').readAsString();
-    final yaml = loadYaml(pubspec);
-  
-    if(true == yaml['dependencies']?.containsKey(riverpodPackageName)) {
-      return;
-    } 
-  } catch (e) {
-    context.logger.err('$e');
-  }
+void run(HookContext context) {
+  // final riverpodPackageName = 'flutter_riverpod';
 
-  final progress = context.logger.progress('Installing $riverpodPackageName Package');
-  await Process.run('flutter', ['pub', 'add', riverpodPackageName]);
-  progress.complete();
+  // try {
+  // final pubspec = await File('./pubspec.yaml').readAsString();
+  // final yaml = loadYaml(pubspec);
+
+  // if(true == yaml['dependencies']?.containsKey(riverpodPackageName)) {
+  //   return;
+  // }
+  // } catch (e) {
+  //   context.logger.err('$e');
+  // }
+
+  // final progress =
+  //     context.logger.progress('Installing $riverpodPackageName Package');
+  // await Process.run('flutter', ['pub', 'add', riverpodPackageName]);
+  // progress.complete();
 }
