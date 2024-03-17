69453c342ba6282e8d8e7e0ff2794a11efb1817a
Nico Klage <nico.klage@gmx.de>
2024-03-17 13:02:30 +0100

    l10n exports added

---
+++ nk_riverpod_feature/__brick__/lib/features/{{feature_name.snakeCase()}}/l10n.dart
@@ -0,0 +1,3 @@
+export 'l10n/{{feature_name.snakeCase()}}_localization.dart';
+export 'l10n/{{feature_name.snakeCase()}}_localization_de.dart';
+export 'l10n/{{feature_name.snakeCase()}}_localization_en.dart';
